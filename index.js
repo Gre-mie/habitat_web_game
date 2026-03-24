bd1582e5dc5c9e5fce0e7d17122564df8ba4e4f2
Gre-mie <114353201+Gre-mie@users.noreply.github.com>
2026-03-24 21:36:11 +0000

    experimenting

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 let habitat = {}
 
-function setHabitat(element) {
-  console.log(`setting habitat to: ${element.name}`)
+function setHabitat(el) {
+
+  console.log(el)
+  console.log(el.dataset)
+
+  let t = "-"
+
+
+  console.log(`habitat: ${el.name}
+chance modifyers:
+- hunt: ${el.dataset.hunt}
+- forage: ${el.dataset.forage}
+- wood: ${el.dataset.wood}
+- stone: ${el.dataset.stone}
+
+- fertility: ${el.dataset.grow}
+- sickness: ${el.dataset.oldage}
+
+- grow: ${el.dataset.fertility}
+- oldage: ${el.dataset.sickness}
+`)
+
+  //window.location.href = "./game.html"
+ // let board = document.getElementById("game")
+
+  //console.log(board)
+
 }
 
