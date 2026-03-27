f4e9df3324daff10d1aa141eb138e6a96676e50b
Gre-mie <114353201+Gre-mie@users.noreply.github.com>
2026-03-27 20:20:20 +0000

    some refactor and starting build area

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ let habitat = Object.fromEntries(params)
 // window elements
 let boardEl = document.getElementById("board")
 let logEl = document.getElementById("log")
-let workerEl = document.getElementById("var-workers")
+let workerEl = document.getElementById("info-workers")
 let workerDisplayEl = document.getElementById("worker-visual-display")
+let buildEl = document.getElementById("info-build")
 
 // other game variables
 let maxPopulation = 50
@@ -26,25 +27,36 @@ function setup() {
   document.getElementById("hr").classList.add(className)
   document.getElementById("evaluate-year").classList.add(className)
 
-  // set visual display elements to reflect starting state of the game
-  updateBasicInfoFields()
-  updateLogFields()
-
   // fill worker string with visual display of max posible workers
   for (let i = 0; i < maxPopulation; i++) {
     workerDisplay.push("𖨆")
   }
-  
-  // calculate and display starting workers
-  workerEl.dataset.workers = calclulateWorkers()
-  updateWorkerDisplays(workerEl.dataset.workers)
 
+  update()
   
-
+  
+ 
+  
+  // display info and logs
+  updateBasicInfoFields()
+  updateLogFields()
 
 }
 setup()
 
+// runs calculations and updates fields across all fields
+function update() {
+
+   // calculate and display workers
+  workerEl.dataset.workers = calclulateWorkers()
+  updateWorkerDisplays(workerEl.dataset.workers)
+
+  // TODO: ADD UPDATE INFO
+
+
+
+
+}
 
 // updates all elements with className to display data
 function updateFieldsByClass(className, data) {
@@ -57,6 +69,7 @@ function updateFieldsByClass(className, data) {
 // update fields to display game state from board element
 function updateBasicInfoFields() {
   let boardData = boardEl.dataset
+  let build = buildEl.dataset
   updateFieldsByClass("info-frames", boardData.frames)
   updateFieldsByClass("info-children", boardData.children)
   updateFieldsByClass("info-adults", boardData.adults)
@@ -64,11 +77,15 @@ function updateBasicInfoFields() {
   updateFieldsByClass("info-food", boardData.food)
   updateFieldsByClass("info-wood", boardData.wood)
   updateFieldsByClass("info-stone", boardData.stone)
+
+  updateFieldsByClass("info-builders", build.builders)
+  updateFieldsByClass("info-shelter", boardData.shelter)
 }
 
 // update fields to display game state from logs element
 function updateLogFields() {
   let log = logEl.dataset
+  let build = buildEl.dataset
   updateFieldsByClass("log-food", log.food)
   updateFieldsByClass("log-gather-meat", log.meat)
   updateFieldsByClass("log-gather-forage", log.forage)
@@ -81,9 +98,26 @@ function updateLogFields() {
   updateFieldsByClass("log-deaths", log.deaths)
   updateFieldsByClass("log-sickness", log.sickness)
   updateFieldsByClass("log-oldage", log.oldage)
+
+  updateFieldsByClass("log-shelter", log.shelter)
+  updateFieldsByClass("log-shack", build.shack)
+  updateFieldsByClass("log-house", build.house)
+  updateFieldsByClass("log-manor", build.manor)
+
 }
 
-// calculates number of workers
+// TODO: ADD DATA AND LOGS TO BE RESET
+// resets varaibles, to be used after the year evaluates
+function newYearSetup() {
+  let boardData = boardEl.dataset
+  let log = logEl.dataset
+  let build = buildEl.dataset 
+
+  boardData.frames++
+
+}
+
+// calculates number of workers 
 function calclulateWorkers() {
   let data = boardEl.dataset
   let workers = parseInt(data.adults)
@@ -104,9 +138,20 @@ function updateWorkerDisplays(workers) {
 }
 
 
+
+
+
 // this function will be called by the "continue" button
 function evaluateYear() {
+  // this function is where most of the logic should be 
+  update()
+
+
   console.log("evaluating Year...") // TEST:
+
+
+
+
 
 
 // do these last
@@ -116,6 +161,11 @@ function evaluateYear() {
 // TODO: CHECK FOR LOOSE CONDITION
 // population <= 0
 
+  newYearSetup()
+
+  // display info and logs
+  updateBasicInfoFields()
+  updateLogFields()
 }
 
 
